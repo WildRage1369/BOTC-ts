800f9fd517e7492aabd47551fced539157660a7f
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-10-05 16:49:29 -0400

    Set up command structure and add command example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from 'dotenv';
 import path from 'path';
+
+import interactionCreate from "./listeners/interactionCreate";
+import ready from "./listeners/ready";
+
 var token;
 const env_config = dotenv.config({ path: path.resolve(__dirname, '../.env') })
 if (env_config.parsed) token = env_config.parsed.CLIENT_TOKEN
 
-console.log("Bot is starting...");
+    console.log("Bot is starting...");
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildModeration,
+        ]
+    });
 
-const client = new Client({
-    intents: []
-});
+    ready(client);
+    interactionCreate(client);
 
-client.login(token);
+    client.login(token);
