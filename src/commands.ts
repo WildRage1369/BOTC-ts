1452dcee3bda48574506036f14e693eb16fdf81c
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-10-07 17:47:59 -0400

    Remove example /hello command

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,6 @@
 import { Command } from "./command";
-import { Hello } from "./commands/hello";
 import { Start } from "./commands/start";
 import { ListPlayers } from "./commands/list_players";
 import { ListRoles } from "./commands/list_roles";
 
-export const Commands: Command[] = [Hello, Start, ListPlayers, ListRoles];
+export const Commands: Command[] = [Start, ListPlayers, ListRoles];
