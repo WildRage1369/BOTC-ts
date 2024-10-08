50616383bcd197b87fc8b4b05d9dd5ed6fcab85d
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-10-08 13:53:18 -0400

    Add proper-lockfile dependency and implement it into any command that writes to a file

--- src/commands/set_script.ts
+++ src/commands/set_script.ts
@@ -3,6 +3,7 @@ import { Command } from "../command";
 import fs from 'fs';
 import path from 'path';
 import interactionCreate from "./../listeners/interactionCreate";
+const lockfile = require('proper-lockfile');
 
 const choices = getScripts();
 
@@ -25,56 +26,68 @@ export const SetScript: Command = {
         }
     ],
     run: async (client: Client, interaction: CommandInteraction) => {
-        const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./../game_state.json"), "utf-8"));
-        var content = "Script set to"
-        if (interaction.options.data[0].value == "new") {
-            json.script = interaction.options.data[1].attachment?.name;
-            const new_script = await interaction.options.data[1].attachment?.toJSON();
-            fs.writeFile(path.resolve(__dirname, "./../scripts/" + json.script), JSON.stringify(new_script), (err) => {
-                console.error(err);
-            })
-            content += " new script " + interaction.options.data[1].attachment?.name
-        } else {
-            json.script = interaction.options.data[0].value;
-            fs.writeFile(path.resolve(__dirname, "./../game_state.json"), JSON.stringify(json), (err) => {
-                console.error(err);
-            })
-            content += " " + interaction.options.data[0].value
-        }
+        var content = "Error: Failed to run command"
+        lockfile.lock(path.resolve(__dirname, "./../game_state.json"))
+            .then((release: Function) => {
+                const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./../game_state.json"), "utf-8"));
+                content = "Script set to"
+                if (interaction.options.data[0].value == "new") {
+                    json.script = interaction.options.data[1].attachment?.name;
+                    const new_script = interaction.options.data[1].attachment?.toJSON();
+                    fs.writeFile(path.resolve(__dirname, "./../scripts/" + json.script), JSON.stringify(new_script), (err) => {
+                        if (err) throw err;
+                    })
+                    content += " new script " + interaction.options.data[1].attachment?.name
+                } else {
+                    json.script = interaction.options.data[0].value;
+                    fs.writeFile(path.resolve(__dirname, "./../game_state.json"), JSON.stringify(json), (err) => {
+                        if (err) throw err;
+                    })
+                    content += " " + interaction.options.data[0].value
+                }
 
-        interactionCreate(client);
-
-        await interaction.followUp({
-            ephemeral: true,
-            content
-        });
+                interactionCreate(client);
+                interaction.followUp({
+                    ephemeral: true,
+                    content: content
+                });
+                return release();
+            }).catch((err: Error) => {
+                interaction.followUp({
+                    ephemeral: true,
+                    content: "Error: Command failed to run. Please try again."
+                });
+                throw err;
+            });
+        console.log(content)
     }
 };
 
 function getScripts(cmd?: Command): any[] {
     const files = fs.readdirSync(path.resolve(__dirname, "./../scripts/"));
     var choices: any[] = [
-        {name: "New script", value: "new"}
+        { name: "New script", value: "new" }
     ]
     for (const file of files) {
-        file.replaceAll("_", " ").replaceAll(".json", "");
-        choices = [...choices, {name: file, value: file}]
+        // file.replaceAll("_", " ").replaceAll(".json", "");
+        choices = [...choices, { name: file, value: file }]
     }
-    if (cmd) { cmd.options = [
-        {
-            name: "script",
-            description: "The script to set to",
-            required: true,
-            type: ApplicationCommandOptionType.String,
-            choices
-        },
-        {
-            name: "newscript",
-            description: "The new script",
-            required: false,
-            type: ApplicationCommandOptionType.Attachment
-        }
-    ]
+    if (cmd) {
+        cmd.options = [
+            {
+                name: "script",
+                description: "The script to set to",
+                required: true,
+                type: ApplicationCommandOptionType.String,
+                choices
+            },
+            {
+                name: "newscript",
+                description: "The new script",
+                required: false,
+                type: ApplicationCommandOptionType.Attachment
+            }
+        ]
     }
     return choices;
 
