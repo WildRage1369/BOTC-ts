5b894365823a793a0388edb07ab6cfd7a8664a4f
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-10-08 13:53:35 -0400

    Implement newgame command to reset the game state

---
+++ src/commands/new_game.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction, Client } from "discord.js";
+import { Command } from "../command";
+import fs from 'fs';
+import path from 'path';
+
+export const NewGame: Command = {
+    name: "newgame",
+    description: "Set up a new game",
+    run: async (_client: Client, interaction: CommandInteraction) => {
+        const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./../game_state.json"), "utf-8"));
+        json.players = [];
+        json.nominations = [];
+        json.isDaytime = true;
+        fs.writeFile(path.resolve(__dirname, "./../game_state.json"), JSON.stringify(json), (err) => {
+            console.error(err);
+        })
+        // await interaction.followUp({
+        //     ephemeral: true,
+        //     content
+        // });
+    }
+};
